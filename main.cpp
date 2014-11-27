208922792169f7f1ebb8d263bc289d3b3e463f60
pc-mist <mist_sx@mail.ru>
2014-11-27 06:14:47 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
-//g
+#include "scaner.cpp"
+
+int main(int argc, char** argv)
+{
+    Scaner scaner;
+    if (!scaner.openFiles(argc, argv))
+        return 0;
+    do
+    {
+        scaner.makeNextLexem();
+    } while (!scaner.isEOF());
+    scaner.closeFiles();
+
+    Parser parser;
+
+    if (!parser.openFiles(argc, argv))
+        return 0;
+    parser.makeXMLTree();
+    return 1;
+}
